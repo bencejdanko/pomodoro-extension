8d41d367eb0c5a070e7d2f9c09d8b6a42b9c714c
Bence Danko <bencejdanko@gmail.com>
2023-02-07 01:31:11 -0800

    Redirect to new homepage on new tab

--- scripts/background.js
+++ scripts/background.js
@@ -1,30 +1,8 @@
-chrome.storage.sync.set({
-  timer: 25,
-  goal: "Work on my project"
-}, function() {
-  console.log("Default values for timer and goal have been set in sync storage.");
-});
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status == "complete") {
     if (tab.url == "chrome://newtab/") {
-      chrome.tabs.update(tabId, {url: "local/block.html"});
-    }
-  }
-});
-
-// Check the status of the timer
-chrome.storage.local.get(['timerActive'], function(result) {
-  if (result.timerActive) {
-    chrome.storage.local.get(['blockedWebsites'], function(result) {
-      var blockedWebsites = result.blockedWebsites;
+      chrome.tabs.update(tabId, {url: "homepage.html"});
       
-      // Check the URL
-      chrome.webNavigation.onCommitted.addListener(function(details) {
-        if (blockedWebsites.includes(details.url)) {
-          chrome.tabs.update({url: 'block.html'});
-        }
-      });
-    });
+    }
   }
 });
